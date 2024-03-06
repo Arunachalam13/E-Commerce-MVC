bd78c433832fb74eddb436f4505293f8ba9bcf34
dineshk281800 <dineshk281800@gmail.com>
2024-03-06 22:19:19 +0530

    Added the Category.cs and updated the bootstrap.css.

---
+++ Bulky/BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using BulkyWeb.Data;
+using BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objListCategories = _db.Categories.ToList();
+            return View(objListCategories);
+        }
+    }
+}
